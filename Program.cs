f807bfdc48bfc4eceb3470f80ff631469c507cb3
XR_Developer <87497178+princeyadav7231236@users.noreply.github.com>
2022-04-12 08:14:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class info
+    {
+        public abstract void getInfo(string a, string b);
+    }
+    public class User: info
+    {
+        public override void getInfo(string a, string b)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.getInfo("Pyari Doctor", "Doremon");
+            Console.ReadLine();
+        }
+    }
+}
